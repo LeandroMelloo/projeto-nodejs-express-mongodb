ffc06dce2b8f4deeb0b9f6cac4e602d68d6b3678
LeandroMelloo <leandromoreno1909@hotmail.com>
2019-04-26 15:48:11 -0300

    ajuste de codigo

--- config/express.js
+++ config/express.js
@@ -1,9 +1,11 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 require('dotenv').config()
 require('../config/db')
 
+app.use(cors())
 app.use(bodyParser.json()) // body-parser
 
 app.use('/api', require('../core/auth/auth.routes'))
